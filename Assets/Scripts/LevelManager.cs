5de949a95d15a434ae624fb94a949c511ae4bfa5
Ethan619 <ethanwaller3@gmail.com>
2024-09-01 19:31:40 +0100

    Arrays are for losers

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,27 +4,46 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour
 {
     [SerializeField] List<LevelPart> parts;
+    [SerializeField] Transform player;       
+    [SerializeField] float spawnDistance = 10f;
+    [SerializeField] int maxSpawnedParts = 3;
+
+    private List<LevelPart> spawnedParts = new List<LevelPart>();
     private LevelPart lastSpawnedPart;
 
     void Start()
     {
         SpawnRandomLevel();
-        SpawnRandomLevel();
-        SpawnRandomLevel();
-        SpawnRandomLevel();
+    }
 
+    void Update()
+    {
+        if (lastSpawnedPart != null && Vector3.Distance(player.position, lastSpawnedPart.GetRightMidpointPosition()) < spawnDistance)
+        {
+            SpawnRandomLevel();
+
+            if (spawnedParts.Count > maxSpawnedParts)
+            {
+                LevelPart partToDestroy = spawnedParts[0];
+                spawnedParts.RemoveAt(0);
+                Destroy(partToDestroy.gameObject);
+            }
+        }
     }
 
     public void SpawnRandomLevel()
     {
-        LevelPart newPart = Instantiate(parts.Random());
-
+        LevelPart newPart = Instantiate(parts[Random.Range(0, parts.Count)]);
         Vector3 spawnPosition = Vector3.zero;
+
         if (lastSpawnedPart != null)
         {
-            spawnPosition = lastSpawnedPart.GetRightMidpointPosition() + (newPart.transform.position - newPart.GetLeftMidpointPosition());
+            spawnPosition = lastSpawnedPart.GetRightMidpointPosition() + 
+                            (newPart.transform.position - newPart.GetLeftMidpointPosition());
         }
+
         newPart.transform.position = spawnPosition;
+        spawnedParts.Add(newPart);
         lastSpawnedPart = newPart;
     }
 }
